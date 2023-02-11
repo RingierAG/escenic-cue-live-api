fe221b2b1e21db53125901401a0a5f0ca8177943
Evdoxos Bekas <eumpek@gmail.com>
2023-02-11 18:58:45 +0100

    removed .build

--- .build/src/config/log.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const env = process.env;
-const logsConfig = {
-  LOG_LEVEL: env.LOG_LEVEL || 'debug',
-  LOG_NAME: 'escenic-cue-live',
-};
-exports.default = logsConfig;
-//# sourceMappingURL=log.js.map
